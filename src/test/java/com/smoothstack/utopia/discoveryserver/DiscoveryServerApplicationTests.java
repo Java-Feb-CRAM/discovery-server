8b05060f1b398240559a88a16de324d2ade43a8b
Rob Maes <robert.maes@smoothstack.com>
2021-03-17 19:06:22 -0500

    initial commit

---
+++ src/test/java/com/smoothstack/utopia/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.utopia.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
