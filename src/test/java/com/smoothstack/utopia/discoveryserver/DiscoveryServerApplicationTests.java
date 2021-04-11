27616b778c69ccd3b6fb6124d55c5aa4b5ba37c1
Rob Maes <robert.maes@smoothstack.com>
2021-04-10 20:29:42 -0500

    please work

--- src/test/java/com/smoothstack/utopia/discoveryserver/DiscoveryServerApplicationTests.java
+++ src/test/java/com/smoothstack/utopia/discoveryserver/DiscoveryServerApplicationTests.java
@@ -2,8 +2,12 @@ package com.smoothstack.utopia.discoveryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(
+  locations = "classpath:application-integrationtest.properties"
+)
 class DiscoveryServerApplicationTests {
 
   @Test
