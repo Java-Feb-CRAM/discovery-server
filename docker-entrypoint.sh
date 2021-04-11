279091013ea7e9f92d40c09024a10831ba83969f
Rob Maes <robert.maes@smoothstack.com>
2021-04-11 13:10:15 -0500

    use an api

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-#export ECS_INSTANCE_IP_ADDRESS=$(/usr/bin/curl --retry 5 --connect-timeout 3 -s ${ECS_CONTAINER_METADATA_URI_V4}/task | /usr/bin/jq .Containers[0].Networks[1].IPv4Addresses[0] -r)
-
-export ECS_INSTANCE_IP_ADDRESS=$(tail -1 /etc/hosts | awk '{print $1}')
+export ECS_INSTANCE_IP_ADDRESS=$(/usr/bin/curl --retry 5 --connect-timeout 3 -s 'https://api.ipify.org?format=json' | /usr/bin/jq .ip -r)
 exec java ${JAVA_OPTS} -Deureka.instance.ip-address=${ECS_INSTANCE_IP_ADDRESS} -jar /app/app.jar "$@"
